794cdb3baaa084e5ade7c35ab51253b0e8c7a5ca
kesavan <kesavanplk@7gmail.com>
2023-03-29 13:50:37 +0530

    add root in index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+import App from "./App";
+
+const root = ReactDOM.CreateRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

b362a71557f6503075084af80f04d77e82ed0122
kesavan <kesavanplk@7gmail.com>
2023-03-29 16:40:21 +0530

    add card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import Card from "./components/card/card.component";
 const App = () => {
-  return <Card />;
+  return (
+    <div className="container">
+      <Card />
+    </div>
+  );
 };
 export default App;
